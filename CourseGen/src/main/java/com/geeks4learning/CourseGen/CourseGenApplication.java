fc47c07b5ead7decda424940e6077176e0b914ac
Thapelo <thapelo.seema@gmail.com>
2024-10-14 12:18:46 +0200

    configured app properties

---
+++ CourseGen/src/main/java/com/geeks4learning/CourseGen/CourseGenApplication.java
@@ -0,0 +1,13 @@
+package com.geeks4learning.CourseGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseGenApplication.class, args);
+	}
+
+}
