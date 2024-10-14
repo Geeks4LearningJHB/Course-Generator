dfff8ab7f7b3be46d77b36f9f19cea24526be838
Thapelo <thapelo.seema@gmail.com>
2024-10-14 12:26:17 +0200

    project setup

--- CourseGen/src/main/java/com/geeks4learning/CourseGen/CourseGenApplication.java
+++ CourseGen/src/main/java/com/geeks4learning/CourseGen/CourseGenApplication.java
@@ -6,8 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CourseGenApplication {
 
+	@Value("${openai.key}") //injects the key from the application.properties file
+	private String apiKey;
 	public static void main(String[] args) {
-		SpringApplication.run(CourseGenApplication.class, args);
+		SpringApplication.run(CourseGeneratorApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		System.out.println("api: " + apiKey);
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getInterceptors().add(((request, body, execution) ->{
+			request.getHeaders().add("Authorization", "Bearer " + apiKey);
+			return execution.execute(request, body);
+		}));
+		return restTemplate;
 	}
 
 }
