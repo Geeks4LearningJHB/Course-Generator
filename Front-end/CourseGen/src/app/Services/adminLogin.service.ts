89fb9071587976271a966105133e4c7eef953285
treasure <lottringt12018@gmail.com>
2024-11-12 09:30:29 +0200

    forgot password

--- Front-end/CourseGen/src/app/Services/adminLogin.service.ts
+++ Front-end/CourseGen/src/app/Services/adminLogin.service.ts
@@ -1,6 +1,6 @@
 // src/app/auth.service.ts
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -9,6 +9,7 @@ import { catchError } from 'rxjs/operators';
 })
 export class LoginService {
   private apiUrl = 'http://localhost:8080/Admin/Adminlogin';
+  private resetPasswordUrl = 'http://localhost:8080/Admin/reset-password';
 
   constructor(private http: HttpClient) {}
 
@@ -20,5 +21,17 @@ export class LoginService {
       })
     );
   }
-  
+
+  resetPassword(email: string, newPassword: string): Observable<any> {
+    const params = new HttpParams()
+      .set('email', email)
+      .set('newPassword', newPassword);
+
+    return this.http.post(this.resetPasswordUrl, {}, { params }).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.error('Reset password error:', error); // Log the error for debugging
+        return throwError('Something went wrong; please try again later.');
+      })
+    );
+  }
 }
--- Front-end/CourseGen/src/app/Trainer/forgot-password/forgot-password.component.ts
+++ Front-end/CourseGen/src/app/Trainer/forgot-password/forgot-password.component.ts
@@ -1,23 +1,39 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../../Services/adminLogin.service';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrl: './forgot-password.component.css'
+  styleUrls: ['./forgot-password.component.css']  // Changed to `styleUrls`
 })
 export class ForgotPasswordComponent {
-onForgetPassword() {
-throw new Error('Method not implemented.');
-}
   forgotPasswordForm: FormGroup;
 
-  constructor(private fb: FormBuilder){
+  constructor(private fb: FormBuilder, private loginService: LoginService) {
     this.forgotPasswordForm = this.fb.group({
-      email: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
-      newPassword: ['', Validators.required]
+      confirmPassword: ['', Validators.required]
     });
   }
 
+  onForgetPassword() {
+    // if (this.forgotPasswordForm.invalid) {
+    //   alert("Please fill in all required fields with valid information.");
+    //   return; 
+    // }
+
+    const { email, password, confirmPassword } = this.forgotPasswordForm.value;
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+
+    this.loginService.resetPassword(email, password).subscribe({
+      next: () => alert('Password reset successful! Please log in with your new password.'),
+      error: (error) => alert('Error resetting password: ' + (error.error.message || 'Please try again later.'))
+    });
+  }
 }
