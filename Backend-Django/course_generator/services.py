f9699fcf2bd70e6579a72b0853646bc9074f7a46
KingBiggie <lottringt12018@gmail.com>
2025-05-12 15:14:03 +0200

    deleted

--- Backend-Django/course_generator/services.py
+++
@@ -1,9 +0,0 @@
-
-from .generator import CourseGenerator  # Your existing generator code
-
-class CourseGenerationService:
-    def __init__(self):
-        self.generator = CourseGenerator()
-    
-    def generate_course(self, topic: str, level: str = "all") -> dict:
-        return self.generator.generate_course(topic, level)
--- Backend-Django/course_generator/urls.py
+++
@@ -1,7 +0,0 @@
-# course_generator/urls.py
-from django.urls import path
-from .views import GenerateCourseView
-
-urlpatterns = [
-    path('generate/', GenerateCourseView.as_view(), name='generate_course'),
-]
